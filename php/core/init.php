e400bf5d0086291e6e678f2744f7528e49988e33
Maquiz1 <manquiz92@gmail.com>
2023-05-31 16:00:11 +0300

    fix new updates

--- php/core/init.php
+++ php/core/init.php
@@ -3,10 +3,10 @@ session_start();
 
 $GLOBALS['config'] = array(
   'mysql' => array(
-      'host' => '127.0.0.1',
+      'host' => 'localhost',
       'username' => 'root',
       'password' => 'Data@2020',
-      'db' => 'filetrack'
+      'db' => 'files'
   	),
   'remember' =>array(
       'cookie_name' => 'hash',
